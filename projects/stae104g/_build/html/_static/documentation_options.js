1563aeddb7bcc16944d3476d796ad6a900f8cb01
bsm@hi.is <bsm@hi.is>
2021-07-07 15:05:24 +0000

    Skipt úr local þema (_themes/) yfir í global og leit lagfærð

--- projects/stae104g/_build/html/_static/documentation_options.js
+++ projects/stae104g/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2018',
+    VERSION: '2021',
     LANGUAGE: 'is',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
