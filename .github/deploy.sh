3ec98552ff363d3494d22074890097c5c239bf6c
Jón Levy <nonni@nonni.cc>
2021-07-27 15:50:00 +0000

    fix rel prod path (#35)

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -16,12 +16,12 @@ ls -d ${GITHUB_WORKSPACE}/${REMOTE_SUB_DIR}/*
 echo "REMOTE_BASE_DIR: $REMOTE_BASE_DIR"
 echo "REMOTE_SUB_DIR: $REMOTE_SUB_DIR"
 
-REMOTE_DIR=$([ "$REMOTE_SUB_DIR" = "prod" ] && echo "" || echo "$REMOTE_SUB_DIR")
+REL_PATH=$([ "$REMOTE_SUB_DIR" = "prod" ] && echo "${GITHUB_WORKSPACE}/${REMOTE_SUB_DIR}/./" || echo "${GITHUB_WORKSPACE}/./${REMOTE_SUB_DIR}")
 
 set -x -e
 rsync \
   -navhizP \
   --stats \
   --progress \
-  --relative "${GITHUB_WORKSPACE}/./${REMOTE_DIR}" \
+  --relative "${REL_PATH}" \
   staging:"${REMOTE_BASE_DIR}/"
