8fe7ae65eb3ad1f7ae3ac7d248f192cfc92aaf89
Jón Levy <nonni@nonni.cc>
2021-07-27 08:52:09 +0000

    Deploy prod draft (#28)
    * ci: improved production pipeline
    * ci: added pre-commit to enforce conventional-commits

---
+++ .github/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+GITHUB_WORKSPACE="$1"
+REMOTE_BASE_DIR="$2"
+REMOTE_SUB_DIR="$3"
+# REMOTE_PROD_DIR=$([ "$REMOTE_SUB_DIR" = "~" ] && echo "" || echo "$GITHUB_WORKSPACE")
+
+echo "#### Moving ${GITHUB_WORKSPACE}/_build/* to ${GITHUB_WORKSPACE}/${REMOTE_SUB_DIR} ####"
+set -x -e
+mkdir -p "${GITHUB_WORKSPACE}/${REMOTE_SUB_DIR}"; mv ${GITHUB_WORKSPACE}/_build/* "$_"
+
+echo "#### Project list: ${GITHUB_WORKSPACE}/${REMOTE_SUB_DIR} ####"
+set -x -e
+ls -d ${GITHUB_WORKSPACE}/${REMOTE_SUB_DIR}/*
+
+echo "REMOTE_BASE_DIR: $REMOTE_BASE_DIR"
+echo "REMOTE_SUB_DIR: $REMOTE_SUB_DIR"
+
+set -x -e
+rsync \
+  -navhizP \
+  --stats \
+  --progress \
+  --relative "${GITHUB_WORKSPACE}/./${REMOTE_SUB_DIR}" \
+  staging:"${REMOTE_BASE_DIR}/"
