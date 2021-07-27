6212d033da1b4fd54232b2577c776f27773086a0
Jón Levy <jon.levy@ruv.is>
2021-07-27 07:35:14 +0000

    ci: set -x -e for output, remove quotes

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -4,15 +4,18 @@ GITHUB_WORKSPACE="$1"
 REMOTE_BASE_DIR="$2"
 REMOTE_SUB_DIR="$3"
 
-echo "Moving ${GITHUB_WORKSPACE}/_build/ to ${GITHUB_WORKSPACE}/${REMOTE_SUB_DIR}"
+echo "#### Moving ${GITHUB_WORKSPACE}/_build/* to ${GITHUB_WORKSPACE}/${REMOTE_SUB_DIR} ####"
+set -x -e
 mkdir -p "${GITHUB_WORKSPACE}/${REMOTE_SUB_DIR}"; mv "${GITHUB_WORKSPACE}/_build/*" "$_"
 
-echo "Project list: ${GITHUB_WORKSPACE}/${REMOTE_SUB_DIR}"
+echo "#### Project list: ${GITHUB_WORKSPACE}/${REMOTE_SUB_DIR} ####"
+set -x -e
 ls -d "${GITHUB_WORKSPACE}/${REMOTE_SUB_DIR}/*"
 
 echo "REMOTE_BASE_DIR: $REMOTE_BASE_DIR"
 echo "REMOTE_SUB_DIR: $REMOTE_SUB_DIR"
 
+set -x -e
 rsync \
   -navhizP \
   --stats \
