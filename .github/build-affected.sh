68da07aa5da56175f5c8f9caff2ee8f1b7edb957
Jon Levy <nonni@nonni.cc>
2023-02-28 07:18:19 +0000

    ci: build affected on both push and pr

--- .github/build-affected.sh
+++ .github/build-affected.sh
@@ -2,7 +2,9 @@
 
 set -euox pipefail
 
-head_sha=${1:-HEAD}
-for i in $( git diff --name-only "$head_sha" origin/main | grep "src/projects" | cut -d "/" -f3 | uniq -u); do
+base_sha=${1:-BASE_SHA}
+head_sha=${2:-HEAD_SHA}
+
+for i in $( git diff --name-only "$base_sha" "$head_sha" | grep "src/projects" | cut -d "/" -f3 | uniq -u); do
   make build project="${i}";
 done
