012d09a5fc59ced9168bfa7051f85d79fd3c6cee
Jon Levy <nonni@nonni.cc>
2023-02-28 07:27:22 +0000

    ci: echo affected projects

--- .github/build-affected.sh
+++ .github/build-affected.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-set -euox pipefail
+set -euo pipefail
 
-base_sha=${1:-BASE_SHA}
-head_sha=${2:-HEAD_SHA}
+base_sha=${1:-origin/main}
+head_sha=${2:-HEAD}
 
-for i in $( git diff --name-only "$base_sha" "$head_sha" | grep "src/projects" | cut -d "/" -f3 | uniq -u); do
+affected=$( git diff --name-only "$base_sha" "$head_sha" | grep "src/projects" | cut -d "/" -f3 | uniq -u)
+echo "Affected projects:"
+echo
+echo "$affected"
+
+for i in $affected; do
   make build project="${i}";
 done
