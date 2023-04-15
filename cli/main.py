998eef1279ef8ab90bb35bd276d62a2f1d6e7177
Jón Levy <nonni@nonni.cc>
2023-04-15 13:04:00 +0000

    chore: move helpers to lib dir

--- cli/main.py
+++ cli/main.py
@@ -3,89 +3,26 @@ import json
 import shutil
 from pathlib import Path
 from subprocess import call
-from typing import List
+from typing import List, Optional
 from enum import Enum
 import typer
 from rich import print
 from rich.console import Console
 from rich.table import Table
 import yaml
-
-
-class SphinxCmd(str, Enum):
-    build = "sphinx-build"
-    autobuild = "sphinx-autobuild"
-
+from cli.lib import (
+    SphinxCmd,
+    get_value,
+    project_exists,
+    build_all,
+    sphinx_build,
+    get_projects_path,
+    get_abs_path,
+    get_template_name,
+)
 
 err = Console(stderr=True)
 app = typer.Typer(rich_markup_mode="rich", name="edbook")
-
-
-# TODO: move all getters to generic settings file
-def get_template_name():
-    return "tmp001g"
-
-
-def get_sphinx_args():
-    return ["-b", "dirhtml"]
-
-
-def get_abs_path(dir: str):
-    return Path.joinpath(Path(__file__).parent.resolve().parent / dir)
-
-
-def get_output_path(dir: str = "_build"):
-    return get_abs_path(dir)
-
-
-def get_projects_path(dir: str = "projects"):
-    return get_abs_path(dir)
-
-
-def get_value(item):
-    if isinstance(item, bytes):
-        return item.decode()
-    return item
-
-
-def sphinx_build(project: str, sphinx_cmd: SphinxCmd = SphinxCmd.build):
-    project_path = Path(get_projects_path() / project)
-    output = Path(get_output_path() / project_path.name)
-    cmd = [
-        sphinx_cmd,
-        str(project_path),
-        str(output),
-        *get_sphinx_args(),
-    ]
-    print(
-        f"[bold purple]Preparing to build {project_path.name}[/bold purple] :sunglasses:"
-    )
-    print(f"[dim light_sea_green]{' '.join(cmd)}[/dim light_sea_green]")
-    call(cmd)
-
-
-def _build_all(ctx: typer.Context, param: typer.CallbackParam, value: str):
-    if value:
-        print(
-            "[bold blue]No project defined, building all projects ...[/bold blue] :boom:"
-        )
-        template = get_template_name()
-        for pr in get_projects_path().iterdir():
-            if pr.name == get_template_name():
-                print(
-                    f"[yellow]Skip building {template} template course ...[/yellow] :fast_forward:"
-                )
-                continue
-            sphinx_build(pr.name)
-        raise typer.Exit()
-
-
-def _project_exists(project: Path):
-    print(project)
-    project = Path(get_projects_path() / project)
-    if project.exists():
-        return project
-    raise typer.BadParameter(f"{project.name} does not exist")
 
 
 @app.callback()
@@ -161,14 +98,14 @@ def cmd_build(
     project: Path = typer.Argument(
         None,
         help="build specific project",
-        callback=_project_exists,
+        callback=project_exists,
     ),
     auto: bool = typer.Option(
         False,
         help="run server on http://localhost:8000 and autobuild and refresh on file save",
     ),
     all: bool = typer.Option(
-        False, help="build all projects", callback=_build_all, is_eager=True
+        False, help="build all projects", callback=build_all, is_eager=True
     ),
 ):
     """
@@ -185,6 +122,7 @@ def cmd_build(
 def cmd_create(
     name: str = typer.Option(..., prompt="Course id"),
     description: str = typer.Option(..., prompt="Course name"),
+    num_authors: List[str] = typer.Option(None),
     author: str = typer.Option(..., prompt="Author name"),
     email: str = typer.Option(..., prompt="Author email"),
     template: str = typer.Option(
