f2c7e6cd951d49c9737e2b69ec12f3b9961e5b85
Jón Levy <nonni@nonni.cc>
2023-04-15 22:12:33 +0000

    chore: cleanup conf.py

--- projects/stae104g/conf.py
+++ projects/stae104g/conf.py
@@ -1,10 +1,1 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-import os, sys
-from datetime import date
-from pathlib import Path
-
-# sys.path.insert(0, os.path.abspath("."))
-# sys.path.append(os.path.abspath("../../settings"))
-
 from edbook.conf import *  # type: ignore
