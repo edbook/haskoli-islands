30487c7bf1a4a0495e19193c6b4c3096858c2be6
Matthías Andri Hrafnkelsson <tfmatti@gmail.com>
2024-05-17 19:12:15 +0000

    fix: updated setting, author data now formatted properly

--- cli/edbook/conf/settings.py
+++ cli/edbook/conf/settings.py
@@ -421,7 +421,7 @@ epub_exclude_files = ["search.html"]
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {"http://docs.python.org/": None}
+intersphinx_mapping = {'python': ("http://docs.python.org/", None)}
 
 
 from docutils import nodes
